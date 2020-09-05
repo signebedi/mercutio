3326f8ccad3733badcbe30df45cffee99664bca1
signebedi <signe@siftingwinnowing.com>
2020-09-05 13:42:36 -0700

    moved to 0.5.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="mercutio",
-    version="0.5.0",
+    version="0.5.1",
     author="Sig Janoska-Bedi",
     author_email="signe@siftingwinnowing.com",
     description="yet another character creation engine",
