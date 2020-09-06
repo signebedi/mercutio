cbd564753afb35de39e477339cdef69f9e1cd449
signebedi <signe@siftingwinnowing.com>
2020-09-06 11:59:11 -0700

    added new default races and subraces

--- src/mercutio/defaults.py
+++ src/mercutio/defaults.py
@@ -17,9 +17,15 @@ attributes_options = [
 race_options = [
     'human', 
     'elf', 
+    'wood elf', 
+    'high elf', 
+    'dark elf', 
     'dwarf', 
-    'orc', 
+    'hill dwarf', 
+    'mountain dwarf', 
     'halfling', 
+    'stout halfling', 
+    'lightfoot halfling', 
 ]
 
 # src: https://1d4chan.org/wiki/Gods_of_Dungeons_and_Dragons#Dragonlance
@@ -142,6 +148,11 @@ default_dimension_options = [
   {
     'name':'mage', # REQUIRED, designate the player identifier that will trigger this buff
     'dimension': 'class', # REQUIRED, designates which dimension the 'name' option falls under
-    'buff': {'strength':-1,'intelligence':3}, # OPTIONAL, designates the character skills or attributes to buff
+    'buff': {'strength':-1,'intelligence':3,}, # OPTIONAL, designates the character skills or attributes to buff
+  },
+  {
+    'name':'human', # REQUIRED, designate the player identifier that will trigger this buff
+    'dimension': 'race', # REQUIRED, designates which dimension the 'name' option falls under
+    'buff': {'all':1,}, # OPTIONAL, designates the character skills or attributes to buff
   },
 ]
