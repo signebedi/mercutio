93a400044f65c6d19b24b1f7a4ac7f024eb6c0e1
signebedi <signe@siftingwinnowing.com>
2020-09-09 12:39:39 -0700

    choosing to be explicit with deploy.sh repository name

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 python setup.py sdist bdist_wheel
-twine upload dist/* --config-file .pypirc # uses project-scoped API token for mercutio
+twine upload dist/* --repository pypi --config-file .pypirc # uses project-scoped API token for mercutio
