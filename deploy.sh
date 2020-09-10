913aaa679d99d125bd9aea772ed75d01d19abeb5
signebedi <signe@siftingwinnowing.com>
2020-09-09 18:27:05 -0700

    added to-do to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 python setup.py sdist bdist_wheel
 twine upload dist/* --repository pypi --config-file .pypirc # uses project-scoped API token for mercutio
+
+# TO DO: 
+## ADD support for version specification (eg. passing version number to setup.py)
+## ADD support for automatic (simultaneous) Github releases
