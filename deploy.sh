77661278911d0ad37c96cca5fb5c8940aee781c0
signebedi <signe@siftingwinnowing.com>
2020-09-11 17:37:14 -0700

    updated deployment and version

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-MESSAGE="0"
+# src https://gist.github.com/mcguffin/746afffa0929ca8e2ea2ba8538776742
 
-VERSION=`cat src/mercutio/_version.py` | grep -q ^__version__ = \"\d.\d.\d\"
+REGEX="[0-9]\.[0-9]\.[0-9]"
+VERSION=`grep -o "$REGEX" src/mercutio/_version.py`
+
+MESSAGE=$(printf "Release of version %s" $VERSION)
 
 DRAFT="false"
 PRE="false"
@@ -18,30 +21,7 @@ fi
 REPO_NAME=$(basename -s .git $REPO_REMOTE)
 REPO_OWNER=$(git config --get user.name)
 
-# get args
-while getopts v:m:b:draft:pre: option
-do
-	case "${option}"
-		in
-		v) VERSION="$OPTARG";;
-		m) MESSAGE="$OPTARG";;
-		b) BRANCH="$OPTARG";;
-		draft) DRAFT="true";;
-		pre) PRE="true";;
-	esac
-done
-if [ $VERSION == "0" ]; then
-	echo "Usage: git-release -v <version> [-b <branch>] [-m <message>] [-draft] [-pre]"
-	exit 1
-fi
-
-# set default message
-if [ "$MESSAGE" == "0" ]; then
-	MESSAGE=$(printf "Release of version %s" $VERSION)
-fi
-
-
-API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "%s","name": "v%s","body": "%s","draft": %s,"prerelease": %s}' "$VERSION" "$BRANCH" "$VERSION" "$MESSAGE" "$DRAFT" "$PRE" )
+API_JSON=$(printf '{"tag_name": "%s","target_commitish": "%s","name": "mercutio %s","body": "%s","draft": %s,"prerelease": %s}' "$VERSION" "$BRANCH" "$VERSION" "$MESSAGE" "$DRAFT" "$PRE" )
 API_RESPONSE_STATUS=$(curl --data "$API_JSON" -s -i https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases?access_token=$GITHUB_ACCESS_TOKEN)
 echo "$API_RESPONSE_STATUS"
 
