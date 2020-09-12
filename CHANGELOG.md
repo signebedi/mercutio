e829b8ce81910737258e062a426f097c3878c6ea
signebedi <signe@siftingwinnowing.com>
2020-09-11 17:02:43 -0700

    automated github releases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,14 +4,6 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-Changes will fall under the following categories:
-- Added for new features.
-- Changed for changes in existing functionality.
-- Deprecated for soon-to-be removed features.
-- Removed for now removed features.
-- Fixed for any bug fixes.
-- Security in case of vulnerabilities.
-
 ## [Unreleased]
 ### Added
 - support for exp, hp, size, speed, spells, attacks, and equipment (default to starting equipment, but allow add'l items to be added) customization
@@ -26,6 +18,10 @@ Changes will fall under the following categories:
 
 ### Changed
 - python-semantic-release to automate [sematic versioning](https://python-semantic-release.readthedocs.io/en/latest/automatic-releases/index.html#automatic) with travis-ci, twine, github tags/releases, and setuptools
+
+## [0.8.4] - 2020-09-11
+### Added
+- automated github release 
 
 ## [0.8.3] - 2020-09-11
 ### Added
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,49 @@
+MESSAGE="0"
+
+VERSION=`cat src/mercutio/_version.py` | grep -q ^__version__ = \"\d.\d.\d\"
+
+DRAFT="false"
+PRE="false"
+BRANCH="master"
+GITHUB_ACCESS_TOKEN=`cat .github_token`
+
+# get repon name and owner
+REPO_REMOTE=$(git config --get remote.origin.url)
+
+if [ -z $REPO_REMOTE ]; then
+	echo "Not a git repository"
+	exit 1
+fi
+
+REPO_NAME=$(basename -s .git $REPO_REMOTE)
+REPO_OWNER=$(git config --get user.name)
+
+# get args
+while getopts v:m:b:draft:pre: option
+do
+	case "${option}"
+		in
+		v) VERSION="$OPTARG";;
+		m) MESSAGE="$OPTARG";;
+		b) BRANCH="$OPTARG";;
+		draft) DRAFT="true";;
+		pre) PRE="true";;
+	esac
+done
+if [ $VERSION == "0" ]; then
+	echo "Usage: git-release -v <version> [-b <branch>] [-m <message>] [-draft] [-pre]"
+	exit 1
+fi
+
+# set default message
+if [ "$MESSAGE" == "0" ]; then
+	MESSAGE=$(printf "Release of version %s" $VERSION)
+fi
+
+
+API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "%s","name": "v%s","body": "%s","draft": %s,"prerelease": %s}' "$VERSION" "$BRANCH" "$VERSION" "$MESSAGE" "$DRAFT" "$PRE" )
+API_RESPONSE_STATUS=$(curl --data "$API_JSON" -s -i https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases?access_token=$GITHUB_ACCESS_TOKEN)
+echo "$API_RESPONSE_STATUS"
+
 python setup.py sdist bdist_wheel
 twine upload dist/* --repository pypi --config-file .pypirc # uses project-scoped API token for mercutio
-
-# TO DO: 
-## ADD support for version specification (eg. passing version number to setup.py)
-## ADD support for automatic (simultaneous) Github releases
