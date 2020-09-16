ea86a03869cd435381204833948ebc0f0f78008b
signebedi <signe@siftingwinnowing.com>
2020-09-16 12:18:19 -0700

    Added: git tags to release notes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,23 @@
 # src https://gist.github.com/mcguffin/746afffa0929ca8e2ea2ba8538776742
 
 # get version number
-REGEX="[0-9]\.[0-9]\.[0-9]"
+REGEX="[0-9]*\.[0-9]*\.[0-9]*"
 VERSION=`grep -o "$REGEX" src/mercutio/_version.py`
 
+# pull relevant commit messages from the git log and write to a temporary file
+git log 0.9.0..0.9.1 --pretty=%B | grep ^Added >  .commits
+git log 0.9.0..0.9.1 --pretty=%B | grep ^Changed >> .commits
+git log 0.9.0..0.9.1 --pretty=%B | grep ^Fixed >> .commits
+git log 0.9.0..0.9.1 --pretty=%B | grep ^Docs >> .commits
+git log 0.9.0..0.9.1 --pretty=%B | grep ^Tests >> .commits
+
+COMMITS=`cat .commits`
+
 # set a standard, simple message -- in the future, summarize from CHANGELOG
-MESSAGE=$(printf "Release of version %s" $VERSION)
+MESSAGE=$(printf "Release of version %s\n%s" $VERSION $COMMITS)
+
+# remove the temporary file
+rm .commits
 
 
 DRAFT="false"
