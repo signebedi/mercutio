decc17233f20d46251a74b270b125b28fce8d0fb
signebedi <signe@siftingwinnowing.com>
2020-09-17 07:32:08 -0700

    Fixed: simpler logic in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,12 @@ REGEX="[0-9]*\.[0-9]*\.[0-9]*"
 VERSION=`grep -o "$REGEX" src/mercutio/_version.py`
 
 # pull relevant commit messages from the git log and write to a temporary file
-git log $(git describe --tags --abbrev=0)..HEAD --pretty=%B | grep -E '^Added|^Removed|^Changed|^Fixed|^Docs|^Tests|' > .commits
-COMMITS=$(cat .commits)
+COMMITS=`git log $(git describe --tags --abbrev=0)..HEAD --pretty=%B | grep -E '^Added|^Removed|^Changed|^Fixed|^Docs|^Tests|'`
 
 # set a standard, simple message -- in the future, summarize from CHANGELOG
 M=$(printf $'Release of version %s\n%s' $VERSION "$COMMITS")
 # sed command brought to you by https://stackoverflow.com/a/1252191
 MESSAGE=`echo "$M" | sed ':a;N;$!ba;s/\n/\<br\/\>/g'`
-
-# remove the temporary file
-rm .commits
 
 DRAFT="false"
 PRE="false"
